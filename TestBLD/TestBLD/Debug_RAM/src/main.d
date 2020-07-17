68e2fbcae2b7accbe4b45abb1dc1cf2b86bbdef2
LapLk <luukhanhlap012@gmail.com>
2020-07-17 07:33:18 +0700

    Add file

---
+++ TestBLD/TestBLD/Debug_RAM/src/main.d
@@ -0,0 +1,43 @@
+src/main.o: ../src/main.c \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/lib_c99.prefix \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/common.prefix \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ewl_c_version.h \
+ C:/Users/tutri/Desktop/TestBLD/TestBLD/include/S32K144.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/stdint.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ansi_parms.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ewlGlobals.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/os_enum.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/cstdint \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/limits_api.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ewl_lib_ext1.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/wchar_t.h \
+ C:/Users/tutri/Desktop/TestBLD/TestBLD/include/S32K144_features.h
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/lib_c99.prefix:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/common.prefix:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ewl_c_version.h:
+
+C:/Users/tutri/Desktop/TestBLD/TestBLD/include/S32K144.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/stdint.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ansi_parms.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ewlGlobals.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/os_enum.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/cstdint:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/limits_api.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/ewl_lib_ext1.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v6.3/arm32_ewl2/EWL_C/include/wchar_t.h:
+
+C:/Users/tutri/Desktop/TestBLD/TestBLD/include/S32K144_features.h:
